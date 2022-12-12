d0a552ba45dbef569cbeb624103ba12fcfb8f963
eylem <119039079+eylemsayin@users.noreply.github.com>
2022-12-12 22:16:00 +0300

    Add project files.

---
+++ SayiTahminEtOyunu/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace SayiTahminEtOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInputNumber = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblInformation = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.Location = new System.Drawing.Point(77, 13);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(375, 23);
+            this.btnStartGame.TabIndex = 0;
+            this.btnStartGame.Text = "OYUNU BAŞLAT";
+            this.btnStartGame.UseVisualStyleBackColor = true;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1-100 arasında bir sayı giriniz";
+            // 
+            // txtInputNumber
+            // 
+            this.txtInputNumber.Location = new System.Drawing.Point(252, 86);
+            this.txtInputNumber.Name = "txtInputNumber";
+            this.txtInputNumber.Size = new System.Drawing.Size(200, 22);
+            this.txtInputNumber.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(77, 287);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(375, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 213);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(375, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(77, 367);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(375, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // lblInformation
+            // 
+            this.lblInformation.AutoSize = true;
+            this.lblInformation.Location = new System.Drawing.Point(94, 219);
+            this.lblInformation.Name = "lblInformation";
+            this.lblInformation.Size = new System.Drawing.Size(0, 16);
+            this.lblInformation.TabIndex = 6;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(94, 373);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 16);
+            this.lblMessage.TabIndex = 7;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnGuess.Location = new System.Drawing.Point(77, 153);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(375, 23);
+            this.btnGuess.TabIndex = 8;
+            this.btnGuess.Text = "TAHMİN ET";
+            this.btnGuess.UseVisualStyleBackColor = false;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblInformation);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtInputNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStartGame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartGame;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInputNumber;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblInformation;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SayiTahminEtOyunu/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayiTahminEtOyunu
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        int _RandomNumber;
+        int _ClickCount = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = 60;
+            btnGuess.Enabled = false;
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            _RandomNumber = rnd.Next(1,101);
+            progressBar1.Value = progressBar1.Maximum;
+            btnGuess.Enabled = true;
+            lblInformation.Text = string.Empty;
+            lblMessage.Text = string.Empty;
+
+        }
+
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            int _TimeElapsed = Math.Abs(progressBar1.Value-60);
+            int _Number = 0;
+            _ClickCount++;
+
+                try
+            {
+                _Number = int.Parse(txtInputNumber.Text);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Sayı girmeyi unuttunuz ya da hatalı giriş yaptınız.");
+            }
+            lblInformation.Text = _Number.ToString();
+            if (_RandomNumber > _Number)
+            {
+                lblInformation.Text += "=> küçük sayı girdiniz.";
+            }
+            else if (_RandomNumber < _Number)
+            {
+                lblInformation.Text += "=> büyük sayı girdiniz.";
+            }
+
+            else 
+            {
+
+                lblInformation.Text += $"TEBRİKLER BİLDİNİZ.";
+                lblMessage.Text += _ClickCount + ". tahminde ve " + _TimeElapsed.ToString() + ".sn'ye de bildiniz.";
+                timer1.Stop();
+                btnGuess.Enabled = false;
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value--;
+        }
+    }
+}
